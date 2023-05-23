e87a74057ae8e8e3f75199fbbfca52c0fdc25681
Victor <victordealmeida5@gmail.com>
2023-05-23 16:46:54 -0300

    Controle While

---
+++ controle/while_1.py
@@ -0,0 +1,18 @@
+#!python3
+# total = 0
+# quant = 0
+# nota = 0
+
+# while nota != -1:
+#     nota = float(input('Informe sua nota ou -1 para sair: ')) 
+#     if nota != -1:
+#         quant += 1 
+#         total += nota
+
+# print(f'Media da turma: {total / quant}')
+
+x = 10
+while x:
+    print(x)
+    x -= 1
+print('Fim')
