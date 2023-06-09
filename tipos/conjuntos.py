c0d7b704fab4f387d627d06697e79e986b376a16
Victor <victordealmeida5@gmail.com>
2023-06-08 23:44:09 -0300

    add pull

--- tipos/conjuntos.py
+++ tipos/conjuntos.py
@@ -2,7 +2,12 @@
 print({1, 2, 3})
 print(type({1, 2, 3}))
  
-conj = {1, 2, 3, 3, 3, 3}
-# print(conj[1])    # conjuntos nao permite utilzar valores indexados 
-print(conj)         # Conjuntos nao imprime valores repetidos 
-print(len(conj))    # Conjuntos nao retorna valores repetidos 
+conj1 = {1, 2, 3, 3, 3, 3}
+conj2 = set([4, 5, 6, 6, 6])
+
+# print(conj1[1])    # conjuntos nao permite acessar elementos a partir de valores indexados 
+print(conj1)         # Conjuntos nao imprime valores repetidos 
+print(len(conj1))    # Conjuntos nao retorna valores repetidos 
+
+print(conj2)         # Conjuntos nao imprime valores repetidos 
+print(len(conj2))    # Conjuntos nao retorna valores repetidos
